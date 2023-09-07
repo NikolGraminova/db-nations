20096766d6fac1e588ba2dcf732773001339faca
NikolGraminova <nbackup619@gmail.com>
2023-09-07 09:57:56 +0200

    Milestone 3

--- src/main/java/org/nations/Main.java
+++ src/main/java/org/nations/Main.java
@@ -1,6 +1,7 @@
-package org.example;
+package org.nations;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,15 +10,23 @@ public class Main {
         String user = "root";
         String password = "Myroot23?";
 
-        String sql1 = "SELECT countries.name as country, countries.country_id as id, regions.name region, continents.name as continent\n" +
-                "FROM countries\n" +
-                "JOIN regions ON countries.region_id = regions.region_id\n" +
-                "JOIN continents ON regions.continent_id = continents.continent_id\n" +
-                "ORDER BY country;\n";
+        String sql1 = """
+                SELECT countries.name AS country, countries.country_id AS id, regions.name AS region, continents.name AS continent
+                FROM countries
+                JOIN regions ON countries.region_id = regions.region_id
+                JOIN continents ON regions.continent_id = continents.continent_id
+                WHERE countries.name LIKE '%' ? '%'\s
+                ORDER BY country;
+                """;
 
         try{
+            Scanner scan = new Scanner(System.in);
             Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Enter a string to filter the results: ");
+            String search = scan.nextLine();
             try(PreparedStatement ps = conn.prepareStatement(sql1)){
+                ps.setString(1, search);
+                System.out.println("COUNTRY - ID - REGION - CONTINENT");
                 try(ResultSet rs = ps.executeQuery()){
                     while (rs.next()){
                         String country = rs.getString("country");
@@ -28,8 +37,7 @@ public class Main {
                     }
                 }
             }
-
-
+            scan.close();
         } catch (SQLException s){
             System.out.println("SQL Connection error.");
             s.printStackTrace();
